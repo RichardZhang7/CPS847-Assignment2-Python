0ef6db5aeebfa52aae2db0671100d9e1089171e5
rICHARD <jufeng.zhang@ryerson.ca>
2020-02-22 13:02:15 -0500

    testing codecov 4

--- sumTest.py
+++ sumTest.py
@@ -5,6 +5,11 @@ class sumTest(unittest.TestCase):
 
     def testSum(self):
         ans = sum.add(2,3)
+        self.assertEqual(5, ans)
+        
+    
+    def productT(self):
+        ans = sum.add(2,3)
         self.assertEqual(6, ans)
         
 
